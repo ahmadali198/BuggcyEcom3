ac7058e62b2fef80a74c6bec8fd84782fbf15f84
ahmadali198 <myidaliahmad321@gmail.com>
2025-07-19 16:40:35 +0500

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { DarkModeProvider } from './context/Darkmodecontext';
+import { BrowserRouter } from 'react-router-dom'; 
 
 console.log("💡 Root rendering started...");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DarkModeProvider>
-      <App />
-    </DarkModeProvider>
+    <BrowserRouter> {/* Wrap your App with BrowserRouter */}
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
+
